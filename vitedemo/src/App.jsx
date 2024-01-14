64a2e1dc29cedcc6e437e5170fe49ac0b913e254
Ali2210 <Alideveloper95@gmail.com>
2024-01-14 11:53:00 +0500

    subcription

--- vitedemo/src/App.jsx
+++ vitedemo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState } from "react";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
@@ -24,6 +25,10 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import PersonIcon from '@mui/icons-material/Person';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import PaymentIcon from '@mui/icons-material/Payment';
+import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
+import RadioButtonCheckedRoundedIcon from '@mui/icons-material/RadioButtonCheckedRounded';
+
+
 
 function App() {
   
@@ -32,9 +37,11 @@ function App() {
       setValue(newValue);
   };
 
-  const [index, setIndex] = useState("one");
-  const subHandleChange = (index, newIndex) =>{
-    setIndex(newIndex);
+
+  const [tabIndex, setTabIndex] = useState("one");
+  const handleTabIndex = (tabIndex,newValue) => {
+
+      setTabIndex(newValue);
   };
 
   const [anchorEl, setAnchorEl] = useState(null);
@@ -55,12 +62,12 @@ function App() {
           <TabContext value={value}>
             <TabList onChange ={handleChange} variant="scrollable"
               scrollButtons="auto" aria-label="tabs" sx={{position : 'relative', top : '-4.1pc', left : '55pc', background: 'transparent'}}>
-                <Tab value="1" sx={{border : '', color : 'white'}} label="Courses" onClick={handleClick} aria-describedby={id}></Tab> <ArrowDropDownIcon sx={{position : 'relative', top : '1.2pc', left : '-1.00pc' , fontSize : 'small'}}/>
-                <Tab value="2" sx={{border : 'none', color : 'white'}} label="Live classes"></Tab>
-                <Tab value="3" sx={{border : 'none', color : 'white'}} label="Settings"></Tab>
-                <Tab value="4" sx={{border : 'none', color : 'white'}} label="Account"></Tab> <ArrowDropDownIcon sx={{position : 'relative', top : '1.2pc', left : '-1.00pc' , fontSize : 'small'}}/>
+                <Tab value="1"  label="Courses" onClick={handleClick} aria-describedby={id} sx={{border : 'none', color : 'white'}}></Tab> <ArrowDropDownIcon sx={{position : 'relative', top : '1.2pc', left : '-1.00pc' , fontSize : 'small'}}/>
+                <Tab value="2"  label="Live classes" sx={{border : 'none', color : 'white'}}></Tab>
+                <Tab value="3"  label="Settings" sx={{border : 'none', color : 'white'}}></Tab>
+                <Tab value="4"  label="Account" sx={{border : 'none', color : 'white'}}></Tab> <ArrowDropDownIcon sx={{position : 'relative', top : '1.2pc', left : '-1.00pc' , fontSize : 'small'}}/>
               </TabList>
-              <TabPanel value={value}>
+              <TabPanel value="1">
                 <Popper id={id} open={open} anchorEl={anchorEl}>
                   <Box sx={{ border: 1, p: 1, bgcolor: 'black', color : 'white', width : '5pc' }}>
                       MUI5
@@ -81,53 +88,68 @@ function App() {
               </TabPanel>
           </TabContext>
         </Appbar>
-        <Typography variant="h5" component="div" sx={{flexGrow : 1, position : 'relative', top : '3pc' , left : '-33pc', fontSize : '30px'}}> <h4>My account</h4> </Typography>
-        <Box sx={{width : '100%', position : 'relative', top : '2pc'}}>
-          <Stack direction="row" spacing={2}>
-            <Button variant="shadow" sx={{background : 'transparent', position : 'relative', top : '3pc', left : '2.6pc'}}> 
-            <Avatar sx={{background : '#28282B', position : 'relative', left : '-0.2pc'}}>
-              <PersonIcon sx={{color : 'yellow'}}/>
-            </Avatar> Account details
-            </Button>
-            <Button variant="shadow" sx={{background : 'transparent', position : 'relative', top : '7pc', left : '-11.3pc'}}>
-             <Avatar sx={{position : 'relative', left : '-0.2pc', background : '#28282B'}}>
-              <AccountBalanceWalletIcon/>
-             </Avatar> Manage subcriptions
-            </Button>
-            <Button variant="shadow" sx={{background : 'transparent', position : 'relative', top : '11pc', left : '-27.8pc'}}>
-             <Avatar sx={{position : 'relative', left : '-0.2pc', background : '#28282B'}}>
-                <PaymentIcon/>
-              </Avatar> Payment details
-            </Button>
-            <Button variant="shadow" sx={{background : 'transparent', position : 'relative', top : '15pc', left : '-41.7pc'}}>
-            <Avatar sx={{position : 'relative', left : '-0.2pc', background : '#28282B'}}>
-              <AccountBalanceWalletIcon/>
-            </Avatar> Manage student sponsorships
-            </Button>
-          </Stack>
-        </Box>
-        <Box sx={{width : '100%', position : 'relative', top : '2pc'}}>
-          <Stack direction="row" spacing={2}>
-            <Box sx={{position : 'relative', left : '30pc', width : '30pc'}}>
-            <FormControl>
-              <InputLabel sx={{color : 'white', position : 'relative', top : '0pc', left : '-18pc'}}> First Name </InputLabel>
-              <TextField id="firstname" type='text' variant="filled" sx={{input : {color : 'white'}, width : '40pc', background : '#36454F', position : 'relative', top : '2pc'}} />
+        <Typography variant="h5" component="div" sx={{position : 'relative', top : '3pc' , left : '-42pc', fontSize : '36px'}}> <h4>My account</h4> </Typography>
+        <Box sx={{width : '300pc', position : 'relative', top : '2pc', left : '0pc', p : 3}}>
+          <Stack direction="row" spacing={8}>
+            <TabContext value={tabIndex}>
+                <TabList onChange={handleTabIndex} orientation="vertical" scrollButtons="auto" aria-label='scrollable auto tabs' TabIndicatorProps={{style : {backgroundColor : 'yellow', left : 0}}}  sx={{position : 'relative', top : '-2pc', height : 800, left : '-2pc' }}>
+                  <Tab value="one" icon={<PersonIcon sx={{color : 'white', position : 'relative', left : '-1pc', fontSize : 'medium', background : '#28282B', borderRadius : 3}}/>} label="Account details" iconPosition='start' sx={{color : 'white', position : 'relative', left : '1.2pc', borderLeft : 0, borderColor : 'divider', borderTop : 0, top : '-1.5pc', borderBottom : 0,borderRight : 0, fontSize : 'medium', textTransform : 'none'}}></Tab>
+                  <Tab value="two" icon={<AccountBalanceWalletIcon sx={{color : 'white', position : 'relative', left : '0pc',  fontSize : 'medium', background : '#28282B', borderRadius : 3}}/>} label="Manage subcriptions" iconPosition='start' sx={{color : 'white', position : 'relative', left : '1.5pc',  borderLeft : 0, borderColor : 'divider', borderTop : 0, top : '-1.2pc', borderBottom : 0,borderRight : 0, fontSize : 'medium',  textTransform : 'none'}}></Tab>
+                  <Tab value="three" icon={<PaymentIcon sx={{color : 'white', position : 'relative', left : '-0.9pc',  fontSize : 'medium', background : '#28282B', borderRadius : 3}}/>} label="Payment Details" iconPosition='start' sx={{color : 'white', position : 'relative', left : '1.5pc',  borderLeft : 0, borderColor : 'divider', borderTop : 0, top : '-1pc', borderBottom : 0, fontSize : 'medium',borderRight : 0,  textTransform : 'none'}}></Tab>
+                  <Tab value="four" icon={<AccountBalanceWalletIcon sx={{color : 'white', position : 'relative', left : '0.3pc',  fontSize : 'medium', background : '#28282B', borderRadius : 3}}/>} label="Manage student sponsorships" iconPosition='start' sx={{color : 'white', position : 'relative', left : '3.5pc',  borderLeft : 0, borderColor : 'divider', borderTop : 0, top : '-1pc', borderBottom : 0,borderRight : 0, fontSize : 'medium', width: '370px', textTransform : 'none'}}></Tab>
+                </TabList>
+                <TabPanel value={"one"}>
+                  <Box sx={{width : '100%', position : 'relative', top : '2pc'}}>
+                    <Stack direction="row" spacing={2}>
+                      <Box sx={{position : 'relative', left : '5pc', width : '30pc', height : '200'}}>
+                        <FormControl>
+                          <InputLabel sx={{color : 'white', position : 'relative', top : '0pc', left : '-18pc'}}> First Name </InputLabel>
+                          <TextField id="firstname" type='text' variant="filled" sx={{input : {color : 'white'}, width : '40pc', background : '#36454F', position : 'relative', top : '2pc'}} />
               
-              <InputLabel sx={{color : 'white', position : 'relative', top : '3pc', left : '-18pc'}}> Last Name </InputLabel>
-              <TextField id="lastname" type='text'  variant="filled" sx={{input : {color : 'white'}, width : '40pc',  background : '#36454F', color : 'white', position : 'relative', top : '5pc'}} />
+                          <InputLabel sx={{color : 'white', position : 'relative', top : '3pc', left : '-18pc'}}> Last Name </InputLabel>
+                          <TextField id="lastname" type='text'  variant="filled" sx={{input : {color : 'white'}, width : '40pc',  background : '#36454F', color : 'white', position : 'relative', top : '5pc'}} />
              
-              <InputLabel sx={{color : '#36454F', position : 'relative', top : '6pc', left : '-19pc'}}> Email </InputLabel>
-              <TextField id="email" type='text'  variant="filled" sx={{input : {color : '#708090'}, width : '40pc', background : '#1B1212', color : 'white', border : '1px', position : 'relative', top : '8pc'}} />
+                          <InputLabel sx={{color : '#36454F', position : 'relative', top : '6pc', left : '-19pc'}}> Email </InputLabel>
+                          <TextField id="email" type='text'  variant="filled" sx={{input : {color : '#708090'}, width : '40pc', background : '#1B1212', color : 'white', border : '1px', position : 'relative', top : '8pc'}} />
              
-              <InputLabel sx={{color : '#36454F', position : 'relative', top : '9pc', left : '-18pc'}}> Password </InputLabel>
-              <TextField id="password" type='password' variant="filled" sx={{ input : {color : '#1B1212'},  width : '20pc',  background : '#1B1212', color : 'white', position : 'relative', top : '11pc', left : '0.2pc'}} />
-              <VisibilityIcon sx={{position : 'relative', top : '8.69pc', left : '17pc', color : '#708090'}}></VisibilityIcon>
+                          <InputLabel sx={{color : '#36454F', position : 'relative', top : '9pc', left : '-18pc'}}> Password </InputLabel>
+                          <TextField id="password" type='password' variant="filled" sx={{ input : {color : '#1B1212'},  width : '20pc',  background : '#1B1212', color : 'white', position : 'relative', top : '11pc', left : '0.2pc'}} />
+                          <VisibilityIcon sx={{position : 'relative', top : '8.69pc', left : '17pc', color : '#708090'}}></VisibilityIcon>
              
-              <Button variant='outline' sx={{position : 'relative', top : '6.1pc',height : '3.1pc', left : '22pc', color : 'yellow', background : 'black', width : '12pc', border : '1px'}}> Change password </Button>
-              <Button variant='outline' sx={{position : 'relative', top : '10pc', left : '1pc', color : 'black', background : 'yellow', width : '12pc'}} > Save changes </Button>
-            
-            </FormControl>
-            </Box>
+                          <Button variant='outlined' sx={{position : 'relative', top : '6.1pc',height : '3.1pc', left : '22pc', color : 'yellow', background : 'black', width : '12pc', border : '1px'}}> Change password </Button>
+                          <Button variant='outlined' sx={{position : 'relative', top : '8pc', left : '0.5pc', color : 'black', background : 'yellow', width : '12pc'}} > Save changes </Button>        
+                        </FormControl>
+                      </Box>
+                    </Stack>
+                </Box>
+                </TabPanel>
+                <TabPanel value={"two"}>
+                <Box sx={{width : '100%', position : 'relative', top : '2pc'}}>
+                    <Stack direction="row" spacing={2}>
+                      <Box sx={{position : 'relative', left : '5pc', top : '-10pc'}}>
+                        <Box variant="outlined" sx={{border : 2, position : 'relative', height : '6pc', width : '40pc', color : 'yellow', borderRadius : 2}}>
+                          <Box sx={{color : 'white', position : 'absolute', left : '2.7pc', top : '1pc'}}> Monthly </Box>
+                          <Box sx={{position : 'absolute', left : '35pc', top : '1pc', color : 'darkgreen'}}> <CheckCircleOutlineRoundedIcon/></Box>  
+                          <Box sx={{position : 'absolute', top : '3pc', left : '1.5pc', color : 'white', fontSize : 'x-large'}}> <RadioButtonCheckedRoundedIcon sx={{color : 'red', fontSize : 'small'}}/> $ 8.99</Box>
+                          <Box sx={{position : 'absolute', top : '3.3pc', left : '7.2pc', fontSize : 'small', color : 'white'}}>/ Month</Box>
+                        </Box>
+                        <Box variant="outlined" sx={{border : 2, position : 'relative', height : '9pc', width : '40pc', color : 'silver', borderRadius : 2, top : '2pc', left : '0pc'}}>
+                          <Box sx={{color : 'white', position : 'absolute', left : '2.7pc', top : '1pc'}}> Yearly </Box>  
+                          <Box sx={{position : 'absolute', top : '3pc', left : '1.5pc', color : 'white', fontSize : 'x-large'}}> $ 99.99</Box>
+                          <Box sx={{position : 'absolute', top : '3.3pc', left : '7.2pc', fontSize : 'small', color : 'white'}}>/ Amum </Box>
+                          <Button variant="outlined" color='warning' sx={{background : 'black', color : 'yellow', position : 'relative', top : '6pc', width : '30pc', border : 1}}> Select plan </Button>
+                        </Box>
+                            <Button variant="outlined" color='warning' sx={{background : 'black', color : 'yellow', position : 'relative', top : '4pc', left : '0pc', width : '20pc', border : 1}}> Cancel Subcription </Button>
+                            <Button variant="outlined" color='warning' sx={{background : 'yellow', color : 'black', position : 'relative', top : '4pc', left : '0.5pc', width : '20pc', border : 1}}> Save </Button>
+                      </Box>
+                    </Stack>
+                  </Box>
+                </TabPanel>
+                <TabPanel value={"three"}>
+                </TabPanel>
+                <TabPanel value={"four"}>
+                </TabPanel>
+            </TabContext>
           </Stack>
         </Box>
       </Box>
